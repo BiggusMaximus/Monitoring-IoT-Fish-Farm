bf1dd1f16ecb2e263835a0efd55acf8b56392409
BiggusMaximus <annastyabagas164@gmail.com>
2023-01-02 20:46:03 +0700

    finishing data acquisition for anova testing

--- Hardware/src/main.cpp
+++ Hardware/src/main.cpp
@@ -7,9 +7,14 @@ void setup()
 {
     Serial.begin(9600);
     pinMode(pH, INPUT);
+    Serial.println("Start");
+    for (uint32_t i = 0; i < 1000; i++)
+    {
+        ph_voltage(read_ph_analog_avg(pH, 1000));
+    }
+    Serial.println("End");
 }
 
 void loop()
 {
-    ph_voltage(read_ph_analog_avg(pH, 10.0));
 }
--- Hardware/src/ph.h
+++ Hardware/src/ph.h
@@ -6,13 +6,12 @@ void read_ph_analog(int pin)
     Serial.println(val);
 }
 
-float read_ph_analog_avg(int pin, float sample)
+float read_ph_analog_avg(int pin, uint32_t sample)
 {
     float val = 0;
     for (uint32_t i = 0; i < sample; i++)
     {
         val += analogRead(pin);
-        delay(10);
     }
     val = val / sample;
     return val;
@@ -20,9 +19,6 @@ float read_ph_analog_avg(int pin, float sample)
 
 void ph_voltage(float val)
 {
-    for (uint32_t i = 0; i < 1000; i++)
-    {
-        float voltage = 23.190407296245567 - (0.031055233908592963 * val);
-        Serial.println(voltage);
-    }
+    float voltage = 23.190407296245567 - (0.031055233908592963 * val);
+    Serial.println(voltage, 4);
 }
